897009d5ce57032cb8106b538208c0e5d85a3bd7
Jesiel Palacios <jopalacioss@unac.edu.co>
2021-01-08 14:09:20 -0500

    Modifica el componente CounterApp

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export const CounterApp = () => {
     return (
-        <div>
+        <div className="container mt-5">
             <h1>Counter en React</h1>
+            <p>Valor: 1</p>
+            <button
+                className="btn btn-primary"
+            >
+                +1
+            </button>
         </div>
     )
 }
